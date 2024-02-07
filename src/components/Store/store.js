274c63137dba63d57234875e68452a98a33894bc
Akash Yadav <akashvinodyadav11@gmail.com>
2024-02-08 00:07:03 +0530

    Store: comment itemSlice

--- src/components/Store/store.js
+++ src/components/Store/store.js
@@ -4,12 +4,12 @@ import pastryItemsSlice from './pastryItemsSlice ';
 import CupCakeItemsSlice from './cupCakeItemsSlice';
 import weddingCakeItemsSlice from './weddingCakeItemsSlice';
 import cartSlice from './cartSlice';
-import itemsSlice from './itemsSlice ';
+// import itemsSlice from './itemsSlice ';
 // import buyCakeItemsSlice from './buyCakeItemsSlice';
 
 const store = configureStore({
   reducer: {
-    items: itemsSlice.reducer,
+    // items: itemsSlice.reducer,
     cakeItems: cakeItemsSlice.reducer,
     pastryItems: pastryItemsSlice.reducer,
     cupCakeItems: CupCakeItemsSlice.reducer,
