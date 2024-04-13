9ba9969c2cc7158377db56fcbaee4c31c7f1d964
akashyadav2000 <akashyadav2000@users.noreply.github.com>
2024-04-13 09:32:37 +0000

    deploy: 58eaba5110cf0ad2d9b90aef25047580d78680ad

---
+++ assets/Product-quh-lJ2R.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-XmaC30p3.js";import o from"./PastryContainer-AhIF0HDD.js";import t from"./WeddingCakeContainer-Upbiqwpo.js";import e from"./CakeContainer-oIlweD5E.js";import n from"./CupCakeContainer-6qYR0jg2.js";function x(){return r.jsxs(r.Fragment,{children:[r.jsx(e,{}),r.jsx(o,{}),r.jsx(n,{}),r.jsx(t,{})]})}export{x as default};
