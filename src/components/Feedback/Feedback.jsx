0b070626a394dbc64df08b661fe5ec44c8bd8cdb
akashyadav2000 <125122328+akashyadav2000@users.noreply.github.com>
2024-04-14 10:34:26 +0530

    flickring issue solve

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -6,51 +6,51 @@ function Feedback() {
 
   return (
     <div class="contact-container">
-      {/* <form
+      <form
         action="https://api.web3forms.com/submit"
         method="POST"
         class="contact-left"
-      > */}
-      <div class="contact-left-title">
-        <h1 className='feedback-ttile'>Feedback Form</h1>
-        <hr className='hr' />
-      </div>
-      <input
-        type="hidden"
-        name="access_key"
-        value="8c8947fa-a38e-4679-be7d-d0dd2267fcbf"
-      />
-      <input
-        type="text"
-        name="name"
-        placeholder="Your Name"
-        class="contact-inputs"
-        required
-      />
-      <input
-        type="email"
-        name="email"
-        placeholder="Your Email"
-        class="contact-inputs"
-        required
-      />
-      <input
-        type="text"
-        name="Product name"
-        placeholder="Product Name"
-        class="contact-inputs"
-        required
-      />
+      >
+        <div class="contact-left-title">
+          <h1 className='feedback-ttile'>Feedback Form</h1>
+          <hr className='hr' />
+        </div>
+        <input
+          type="hidden"
+          name="access_key"
+          value="8c8947fa-a38e-4679-be7d-d0dd2267fcbf"
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="Your Name"
+          class="contact-inputs"
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          class="contact-inputs"
+          required
+        />
+        <input
+          type="text"
+          name="Product name"
+          placeholder="Product Name"
+          class="contact-inputs"
+          required
+        />
 
-      <textarea
-        name="message"
-        placeholder="Your Message"
-        class="contact-inputs"
-        required
-      ></textarea>
+        <textarea
+          name="message"
+          placeholder="Your Message"
+          class="contact-inputs"
+          required
+        ></textarea>
 
-      <button>Submit</button>
-      {/* </form> */}
+        <button type="submit">Submit</button>
+      </form>
       <div class="contact-right">
         <img src="./Images/flower.jpg" alt="" />
       </div>
